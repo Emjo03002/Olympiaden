e59487bf3e64459bcc4ef23d96bb0df3d5c15dcb
Emjo03002 <70193931+Emjo03002@users.noreply.github.com>
2020-11-27 10:01:19 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,41 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            int antaläpplen = 0;
-            int Axel = 0;
-            int antalpäron = 0;
-            int Petra = 0;
-
-            Console.Write("Hur många äpplen har Axel sålt?");
-            Console.Write("Svar:");
-            antaläpplen = int.Parse(Console.ReadLine());
-            Console.Write("Hur många päron har Petra sålt?");
-            Console.Write("Svar:");
-            antalpäron = int.Parse(Console.ReadLine());
-
-            Axel = antaläpplen * 7;
-            Petra = antalpäron * 13;
-
-            if (Axel != Petra)
-                Console.WriteLine("Lika");
-            else if(Axel < Petra)
-                Console.WriteLine("Petra"); 
-            else
-                Console.WriteLine("Axel");
-
-            Console.ReadLine();
-                
-
-                
+            Console.WriteLine("Skriv in ditt tio siffriga personnummer och få ut det i tolvsiffrig form.");
+            string personnummer = Console.ReadLine();
 
             
+
         }
     }
 }
